a024084f047c543b7f995899d00c55b6a308828f
AndreyLP <pifagor174@yandex.ru>
2025-02-06 23:47:16 +0300

    Add a Message Outbox

--- src/AuctionService/Data/AuctionDbContext.cs
+++ src/AuctionService/Data/AuctionDbContext.cs
@@ -1,5 +1,7 @@
 using AuctionService.Entities;
+using MassTransit;
 using Microsoft.EntityFrameworkCore;
+// using MassTransit;
 
 namespace AuctionService.Data;
 public class AuctionDbContext : DbContext
@@ -8,4 +10,13 @@ public class AuctionDbContext : DbContext
     {
     }
     public DbSet<Auction> Auctions { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.AddInboxStateEntity();
+        modelBuilder.AddOutboxMessageEntity();
+        modelBuilder.AddOutboxStateEntity();
+    }
 }
--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,6 +1,6 @@
 using AuctionService.Data;
-using Microsoft.EntityFrameworkCore;
 using MassTransit;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +14,13 @@ builder.Services.AddDbContext<AuctionDbContext>(opt =>
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddMassTransit(x =>
 {
+    x.AddEntityFrameworkOutbox<AuctionDbContext>(o =>
+    {
+        o.QueryDelay = TimeSpan.FromSeconds(10);
+        o.UsePostgres();
+        o.UseBusOutbox();
+    });
+
     x.UsingRabbitMq((contex, cfg) =>
     {
         cfg.ConfigureEndpoints(contex);
